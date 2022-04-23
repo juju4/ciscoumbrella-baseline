1776555dde3fa54686ee66859050ad27ff162717
juju4 <juju4@users.noreply.github.com>
2022-04-23 21:50:12 +0000

    revalidate controls

--- controls/umbrella.rb
+++ controls/umbrella.rb
@@ -5,7 +5,7 @@
 
 title 'Umbrella section'
 
-umbrella_org_id = input('umbrella_org_id', value: '', description: 'Check Umbrella use the correct Org ID')
+umbrella_org_id = input('umbrella_org_id', value: false, description: 'Check Umbrella use the correct Org ID')
 umbrella_client = input('umbrella_client', value: false, description: 'Is Umbrella setup with client or anyconnect')
 umbrella_client_file = input('umbrella_client_file', value: '/opt/cisco/anyconnect/umbrella/OrgInfo.json', description: 'Umbrella client OrgInfo file path')
 
@@ -18,8 +18,9 @@ control 'umbrella-1.0' do
     describe command('dig +time=10 debug.opendns.com txt') do
       its('stdout') { should_not match 'Error' }
       its('stderr') { should_not match 'Error' }
-      its('stdout') { should match 'organization id' }
-      its('stdout') { should match 'dnscrypt enabled' }
+      if umbrella_client
+        its('stdout') { should match 'dnscrypt enabled' }
+      end
       if umbrella_org_id
         its('stdout') { should match "organization id #{umbrella_org_id}" }
       end
@@ -28,8 +29,9 @@ control 'umbrella-1.0' do
     describe command('nslookup -type=txt debug.opendns.com.') do
       its('stdout') { should_not match 'Error' }
       its('stderr') { should_not match 'Error' }
-      its('stdout') { should match 'organization id' }
-      its('stdout') { should match 'dnscrypt enabled' }
+      if umbrella_client
+        its('stdout') { should match 'dnscrypt enabled' }
+      end
       if umbrella_org_id
         its('stdout') { should match "organization id #{umbrella_org_id}" }
       end
@@ -40,9 +42,9 @@ control 'umbrella-1.0' do
     its('body') { should include 'Welcome to Umbrella!' }
     its('headers.Content-Type') { should cmp 'text/html' }
   end
-  describe http('https://examplemalwaredomain.com', ssl_verify: false) do
-    its('status') { should cmp 403 }
-    its('body') { should include 'location.replace("https://malware.opendns.com' }
+  describe http('https://www.examplemalwaredomain.com', ssl_verify: false) do
+    its('status') { should cmp 200 }
+    its('body') { should include 'examplemalwaredomain.com is only a demonstration site.' }
     its('headers.Content-Type') { should include 'text/html' }
   end
 end
